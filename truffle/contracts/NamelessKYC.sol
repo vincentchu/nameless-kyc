8c88084954bf2a56e6f5dc2f244be4e7b4b6dbcb
Vincent Chu <vincentchu@gmail.com>
2018-05-25 22:27:39 -0300

    Initialize bloomState()

--- truffle/contracts/NamelessKYC.sol
+++ truffle/contracts/NamelessKYC.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.4.23;
 
 contract NamelessKYC {
+    uint32 public bloomState;
 }
--- truffle/test/NamelessKYCTest.js
+++ truffle/test/NamelessKYCTest.js
@@ -1,7 +1,10 @@
 const NamelessKYC = artifacts.require('./NamelessKYC.sol')
 
 contract('NamelessKYC', () => {
-  it('should be deployed', async () => {
+  it('should be deployed with base state', async () => {
     const contract = await NamelessKYC.deployed()
+    const bloomState = await contract.bloomState()
+
+    assert.deepEqual(bloomState.toNumber(), 0)
   })
 })
